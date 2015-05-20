b5e7a54d51e21ed26b8ae6a99ab30baf7dcb32c0
AriellaYamada <ariella.y.b@hotmail.com>
2015-05-20 16:44:26 -0300

    Inicio

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ariellayamada on 20/05/15.
+ */
+public class Book {
+
+    public String Title;
+    public String Author;
+    public String Editor;
+    public int Year;
+    
+}
--- src/Loan.java
+++ src/Loan.java
@@ -1,5 +1,5 @@
 /**
  * Created by ariellayamada on 20/05/15.
  */
-public class User {
+public class Loan {
 }
