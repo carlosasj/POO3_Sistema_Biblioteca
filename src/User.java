de2b4355f398fe8f06d193529ec71d070d5e94e7
Carlos Schneider <carlosaschjr@gmail.com>
2015-05-20 17:43:49 -0300

    Classe User

--- src/User.java
+++ src/User.java
@@ -1,2 +1,27 @@
-public class User {
+abstract class User {
+	private String Name;
+	public Loan[] Loans;
+	private int TotalLoans;
+	private int MaxLoans;
+
+	public String getName(){
+		return Name;
+	}
+
+	public int getTotalLoans(){
+		return TotalLoans;
+	}
+
+	public void newLoan(Loan l){
+
+	}
+
+	public void GiveBackLoan(Book b){
+
+	}
+
+	public void GiveBackLoan(String Title){
+
+	}
+
 }
