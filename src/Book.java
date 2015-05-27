e6fa4122ecc7d99a7c46dec138f59eebeaff6e34
Carlos Schneider <carlosaschjr@gmail.com>
2015-05-26 21:07:53 -0300

    Atualiza Gitignore e inclui o Main

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,6 @@
 public class Book {
 
+    public int ID;
     public String Title;
     public String Author;
     public String Editor;
--- src/Books.java
+++ src/Books.java
@@ -1,5 +1,38 @@
-/**
- * Created by CarlosAlberto on 26/05/2015.
- */
-public class Books {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Books implements Database {
+	BufferedReader br = null;
+
+	public void OpenFile(String path){
+		if (path == null) {
+			path = "/books.csv";
+		}
+
+		try {
+			br = new BufferedReader(new FileReader(path));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void ReadFile(){
+		String line = null;
+		String splitSign = ",";
+
+		try {
+			while ((line = br.readLine()) != null){
+				String[] readed = line.split(splitSign);
+
+				for (String s : readed){
+					System.out.println(s);
+				}
+				System.out.println("-- End\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
