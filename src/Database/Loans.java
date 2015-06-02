29f848978813971e53f8ffd83746f5aceead8025
AriellaYamada <ariella.y.b@hotmail.com>
2015-06-01 23:42:25 -0300

    Teste verificacao emprestimo

--- src/Database/Loans.java
+++ src/Database/Loans.java
@@ -49,13 +49,18 @@ public class Loans {
 		Book book = Books.getInstance().Search();
 
 		// Verifica se o livro pode ser emprestado
+		String confirm = "n";
 		if (book == null || !book.canLoan()) {
 			return;
+		} else if (user.getType().equals("community")) {
+			if (book.getType().equals("text")) {
+				out.println("Usuario nao pode emprestar esse tipo de livro");
+			} else {
+				confirm = scan.nextLine();
+			}
+		} else {
+			
 		}
-
-		out.println("Deseja inserir esse emprestimo? [s|n]");
-		String confirm = scan.nextLine();
-
 		if (confirm.toLowerCase().equals("s") || confirm.equals("")) {
 			Add(nextID, book.getID(), user.getID());
 			nextID++;
@@ -64,7 +69,6 @@ public class Loans {
 		else {
 			out.println("Emprestimo nao cadastrado.");
 		}
-
 	}
 
 	// Repassa os parametros para o Load e escreve no Log
